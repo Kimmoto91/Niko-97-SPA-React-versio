508a07b46b6dde08b5a9588bf8d2e735031891a3
Petteri Sylvänne <petteri.sylvanne@metropolia.fi>
2020-03-15 14:00:22 +0200

    Sosiaalisuus paketit
    Sosiaalisuus paketin tiedot ja linkit lisätty sivulle.
    Ei kyselyä

--- src/containers/InfoKit/Sosiaalisuus.js
+++ src/containers/InfoKit/Sosiaalisuus.js
@@ -10,14 +10,152 @@ class Sosiaalisuus extends Component {
           return (
             <Aux>
             <div className="Home">
-              <h2>Sosiaalisuus</h2>
+              <h1>Sosiaalisuus</h1>
 
-                  <p>Tunnetaidot</p>
-                  <p>Sosiaaliset suhteet</p>
-                  <p>Minäkuva</p>
+                  <h2>Tunnetaidot</h2>
+                  <h3>TUNNETAIDOT – Mitä ne ovat?</h3>
+                  <p>Tunnetaidot voidaan jakaa pääosin kahteen eri osa-alueeseen, tunteiden ilmaisu ja tunteiden säätely.
+                    Tunteiden ilmaisu on kykyä ilmaista omia tunteitaan oikealla tavalla ja tunteiden säätely taas kykyä hallita omia tunteitaan oikealla tavalla.
+                    Näiden lisäksi tunnetaitoihin voidaan lukea myös kyky tunnistaa omat tarpeensa ja kyky hillitä omaa käytöstä.</p>
+                    
+                  <h3>Miksi on hyvä osata käsitellä tunteitaan oikealla tavalla?</h3>
+                  <p>Kyky ymmärtää tunteiden syitä ja seurauksia on suuri voimavara selvitä erilaisista elämän valinnoista, niin arkisista kuin suuristakin päätöksistä.
+                    Kaikki koetut tunteet ovat osa meitä ja vaikeimpienkin tunteiden käsitteleminen ja julki tuominen luovat ihmisestä kokonaisen.
+                    On tärkeää näyttää hyviä tunteita ulospäin ja pystyä käsittelemään negatiivisia tunteita ennen niiden muuttumista vahingoittaviksi.
+                    Jos emme pysty säätelemään omia tunteita, voi helposti ajautua niiden vietäväksi tai jäädä vellomaan hankaliin tunnetilanteisiin.
+                    Hyvät tunnetaidot auttavat meitä selviämään helpommin pettymyksistä ja vastoinkäymisistä, sekä lisäävät itseluottamusta ja parantavat minäkuvaa.</p>
 
+                  <h3>Miten saavutan hyvät tunnetaidot?</h3>
+                  <p>Onko sinulle koskaan sanottu ”hillitse nyt itsesi” tai jotain muuta tämän suuntaista? Itsehillintä on olennainen osa tunteiden säätelyä.
+                    Tämä tarkoittaa sisäistä kontrollia ja kykyä hallita omaa käytöstä iän edellyttämällä tavalla.
+                    Monesti kamppailemme impulssien, mielihalujen ja tunteiden kanssa, ja juuri näihin hetkiin tarvitsemme itsehillintää.
+                    Tilanteessa, joissa tunteet ja mielihalut alkavat ottaa valtaa, on hyvä rauhoittua ja miettiä tilannetta uudestaan ensireaktion laantuessa.
+                    Erityisesti vaikeissa tilanteissa puhuminen jollekin toiselle voi auttaa käsittelemään omia tunteitaan paremmin.
+                    Myös empaattinen kuunteleminen on osa tunnetaitoja ja tämä vaatii kuuntelukyvyn lisäksi kykyä olla todellisesti läsnä.
+                    Hyvä kuuntelija ymmärtää mitä toinen sanoo, muistaa sen ja reagoi siihen mitä on juuri kuullut.</p>
+                  
+                  <h3>LISÄÄ TUNTEISTA JA TUNNETAIDOISTA VOIT LUKEA ESIMERKIKSI <a href="https://mieli.fi/fi/mielenterveys/itsetuntemus/tunteet">TÄÄLTÄ</a></h3>
+                
+
+                  <h2>Minäkuva</h2>
+                  <h3>MINÄKUVA – Terveen minäkuvan luominen</h3>
+                  <p>Minäkuva on meidän oma näkemyksemme itsestämme ja käsitys siitä millainen minä olen.
+                    Minäkuva muuttuu koko elämämme ajan ja se voi vaihtua eri tilanteissa, sillä jokaisella meillä on omanlainen käsitys itsestämme esimerkiksi perheenjäsenenä,
+                    opiskelijana, ystävänä tai työntekijänä. Joskus minäkuvasta voi kehittyä kielteinen, jolloin pelko epäonnistumisista saa meidät välttelemään haasteita tai jopa sosiaalisia kontakteja.
+                    Tällöin alkaa helposti elämään varauksellisesti jokaisella elämän osa-alueella. Terveen minäkuvan perustana on itsearvostus, joka koostuu itsensä hyväksymisestä, itsekunnioituksesta, uskosta omiin kykyihin, tyytyväisyydestä omaan elämään ja kyvystä rakastaa itseään.
+                    Elämässä kokemamme kielteiset ja myönteiset kokemukset muokkaavat itsetuntoa joko hyväksi tai huonoksi. Elämän merkitykselliset kokemukset, mahdollisuus vaikuttaa omaan elämäänsä, arvostus omasta olemassaolostaan ja kyky huomioida omat tarpeensa auttaa meitä rakastamaan itseämme.
+                    Omien tarpeiden kuuntelu on jokaiselle tärkeä taito. Jämäkkyys tarkoittaa vastuuta omasta elämästä ja sen avulla huolehdimme omista rajoista ja opimme olemaan terveellä tavalla itsekkäitä. Toisten asettamiin tavoitteisiin, malleihin ja tehtäviin ei tarvitse aina suostua, sillä jokaisella meistä on oikeus määritellä omat rajamme. Jämäkkyydellä voi vaikuttaa omaan hyvinvointiin ja elämänlaatuun oppimalla antamaan itselleen luvan tehdä virheitä ja omistamaan omia mielipiteitä. On tärkeä kuitenkin muistaa, että jämäkkyys ei ole aggressiivista, vaan sosiaalista vuorovaikutusta. Kyse ei siis ole muiden lyttäämisestä, vaan oikeudesta vetää raja omien ja muiden tarpeiden välille, jolloin pyrimme pitämään kiinni omista oikeuksistamme ja puolustamaan niitä loukkaamatta kuitenkaan toisen osapuolen oikeuksia.</p>
+
+                  <h3>USKON ITSEENI JA KYKYIHINI, OLEN ARVOKAS!</h3>
+
+                  <h3>Sosiaaliset suhteet</h3>
+
+                  <h3>SOSIAALISET SUHTEET – Vuorovaikutustaidot</h3>
+                  <p>Sosiaaliset suhteet ovat ihmisten välisiä suhteita, joiden perustana toimivat normit, arvot ja asenteet.
+                    Sosiaalisilla suhteilla on meidän jokaisen elämässä suuri merkitys ja se on yksi psyykkiseen ja fyysiseen terveyteen vaikuttavista tekijöistä. 
+                    Kaikilla meillä on omanlaisemme tarve kohdata muita ihmisiä ja muodostaa erilaisia ystävyyssuhteita.
+                    Jotkut kaipaavat enemmän ihmisiä ympärilleen ja toiset viihtyvät omissa oloissaan paremmin, mutta yhteistä meille kaikille on se, että kaipaamme ihmiskontaktia edes jossain määrin.
+                    Määrä ei siis korvaa laatua ja jopa vain yksi hyvä ihmissuhde voi antaa elämäämme tarpeeksi tukea, apua ja arvostusta.</p>
+                  <p>Sosiaaliset suhteet tarvitsevat toimiakseen vuorovaikutustaitoja, kuten toisen kuunteleminen, muilta kysyminen ja itsensä esitteleminen.
+                    Hyvä ihmissuhde vaatii molemminpuolista läsnäoloa.
+                    Hyvä ystävä esimerkiksi kykenee samaistumaan toisen tunteisiin ja tunnistaa erilaisia tunteita eleiden ja ilmeiden pohjalta.
+                    Tätä voidaan kutsua myös empatiaksi.  Empaattinen ystävä on läsnä, kuuntelee ja yrittää ymmärtää mitä tunteita/ajatuksia toinen käy läpi.
+                    Joskus on kuitenkin vaikea kuunnella ja olla läsnä toista varten, jos itsestä tuntuu, ettei omatkaan tunteet ole selvillä tai jos jokin asia ei vain tunnu olevan kohdillaan.
+                    Monesti saamme aikaan myös riitoja muiden ihmisten kanssa, sillä jokainen meistä on erilainen ja omistamme omat mielipiteemme.
+                    Jokaisella meillä on myös oikeus näihin omiin tunteisiin ja ajatuksiin.
+                    Kun huomaamme vaikeuksia ihmissuhteissamme, on tärkeää pysähtyä ja miettiä miksi tunnen näin ja mistä kaikki sai alkunsa.
+                    Toisen kanssa kommunikointi eli asioista puhuminen on monesti avain parempaan ymmärrykseen ja suhteeseen.
+                    Joskus voi olla hyvä puhua myös jollekin ulkopuoliselle ihmiselle tilanteesta, jota käyt läpi.</p>
+                  <p>Terveen sosiaalisen suhteen luomiseen tarvitsemme myös luottamusta. Luottamus kuuluu meidän perustarpeisiimme ja se kehittyy jo vauvaiässä.
+                    Vanhempiesi ja muiden läheisten antama tuki lapsuudessasi on syy, miksi perusluottamus on syntynyt. Kun itkit, joku nosti sinut ylös ja rauhoitteli.
+                    Kun taas olit nälkäinen, joku antoi sinulle ruokaa. Lapselle on tärkeää tuntea, että häntä ei hylätä ja jätetä yksin.
+                    Joissain tapauksissa ihminen ei ole lapsuudessa kuitenkaan saanut tarvittavaa tukea ja rakkautta, ja siksi monilla voi vielä aikuisiällä olla suuria vaikeuksia luottaa toiseen ihmiseen.
+                    Perusluottamuksen rakentaminen myöhemmällä iällä on vaikeampaa, mutta luottamusta voi kuitenkin harjoitella jonkun läheisen ihmisen kanssa.
+                    Joskus täytyy vain antaa toiselle mahdollisuus olla luottamuksen arvoinen ja vastavuoroisesti olla sitä myös itse.
+                    Ihmisten ollessa epätäydellisiä luottamus joskus kuitenkin rikkoutuu. Tämä on vaikea saada takaisin, mutta ei suinkaan mahdotonta.
+                    Menetetyn luottamuksen voi korjata, jos molemmat osapuolet antavat siihen aikansa ja tahtovat korjata sen, mitä on menetetty.
+                    Luottamus on yksi tärkeimmistä hyvän ihmissuhteen rakentumisen edellytyksistä ja se on vahvasti yhteydessä mielemme hyvinvointiin.</p>
+
+                  <h3>Yksinäisyys – tuttu tunne?</h3>
+                  <p>Tunnetko olosi useinkin yksinäiseksi? Et ole tämän tunteen kanssa yksin, sillä monet meistä kokee yksinäisyyttä jossain vaiheessa elämää.
+                    On täysin normaalia, jos esimerkiksi parisuhteen loppumisen, läheisen menettämisen tai työttömäksi joutumisen jälkeen tunnemme jääneemme jollain tavalla yksin.
+                    Myös rahahuolet, ongelmat koulussa, sekä oma tai läheisen alkoholin liikakäyttö ovat isoja ja vaikeita asioita käsitellä.
+                    Ei siis ihme, jos joskus tuntuu, että olen täysin yksin tässä maailmassa.
+                    Tällaisissa tilanteissa alamme helposti sulkea muita ihmisiä ympäriltämme pois, sillä joskus se voi tuntua helpommalta kuin kertoa toisille huolistamme.
+                    Yksinäisyyden kanssa ei kuitenkaan koskaan tarvitse jäädä yksin ja siksi olisikin hyvä puhua vaikeistakin asioista jollekin läheiselle.
+                    Aina ei kuitenkaan ole helppoa puhua ystäville tai perheelle, mutta se ei tarkoita, etteikö maailmassa olisi ketään, joka voisi auttaa ja kuunnella.
+                    Suomessakin on tarjolla monenlaista apua juuri sinulle, joka kamppailet näiden tunteiden kanssa.</p>
+
+                  <p>Esimerkiksi Kohtaus ry tarjoaa nuorille aikuisille mahdollisuuksia itsensä toteuttamiseen, ihmisten tapaamisen ja vapaa-ajan viettoon.
+                    Kaupunkiolohuone on tarkoitettu sinulle 18-30 vuotias Helsingissä asuva nuori, joka kaipaat tulla kohdatuksi turvallisessa ympäristössä.
+                    Kohtays Ry:n sivuilta löydät myös kalenterin, johon on merkitty erilaisia järjestettyjä tapahtumia.
+                    Kuten miesten vuoro tapahtumasarja, joka on tarkoitettu kaikille mieheksi itsensä kokeville ja jossa keskustellaan esimerkiksi ihmissuhteista, seksuaalisuudesta ja miesten rooleihin liittyvistä asioista.</p>
+
+                  <a href="https://www.kphtaus.info">KOHTAUS RY</a>
+
+                  <h3>Minkälainen on terve parisuhde?</h3>
+
+                  <p>Seurustelu on kahden ihmisen välinen suhde, jossa tunnetaan voimakasta henkistä yhteenkuuluvuutta ja rakkauden tunnetta toista kohtaan.
+                    Jokaisen parisuhde on erilainen, mutta hyvä parisuhde perustuu aina välittämiseen, rakastamiseen ja molempien osapuolten vapaaseen tahtoon.
+                    Hyvässä suhteessa kumppani tuo meille turvaa ja vakautta, sekä vahvistaa minäkuvaa ja itsetuntoa.
+                    Kuten kaikki ihmissuhteet, myös vahva parisuhde syntyy välittämisestä, hyvästä kommunikaatiosta ja etenkin molemminpuolisesta luottamuksesta.
+                    Sinun ja kumppanisi tulisi olla varmoja siitä, että molemmat ovat halukkaita antamaan toiselle aikaa ja huomiota, sekä on sitoutunut kestämään erimielisyydet ja haasteet, joita väistämättä parisuhteessa tulee esille.
+                    Tämä ei koskaan kuitenkaan tarkoita sitä, että toisen tarvitsee tulla kohdatuksi huonosti.
+                    Erimielisyydet ovat jokaisen arkea, mutta niiden oikealla tavalla käsittely vaatii hyviä vuorovaikutus – ja tunnetaitoja.
+                    Vanha, mutta hyvä sääntö onkin, että kohtelen toista samalla tavalla kuin haluaisin itseäni kohdeltavan.
+                    Jokaisella meillä on myös oikeus vetää omat rajamme, joita toisen kuuluu kunnioittaa.
+                    Hyvässä parisuhteessa tunnemme toisistamme kiitollisuutta, annamme ja vastaanotamme kiintymyksen osoituksia, sekä pystymme puhumaan kaikesta avoimesti kumppanillemme.</p>
             </div>
-              <div>Palvelunohjaus</div>
+              <div>
+                <h2>Palvelunohjaus</h2>
+
+                <h2><strong>HALUATKO JUTELLA JONKUN KANSSA LISÄÄ?</strong></h2>
+                <p>Voit soittaa esimerkiksi näihin numeroihin. Älä jää murheittesi kanssa yksin!</p>
+              
+                <strong>Nuorten kriisipuhelin</strong> – Surua tai pahaa oloa?
+                <ul>
+                    <li>Maksutonta ja luottamuksellista keskusteluapua 12-29 -vuotiaille nuorille</li>
+                    <li>p. 045 3410 583</li>
+                    <li><a href="https://www.helsinkimissio.fi/nuorten-kriisipiste">Nuorten kriisipiste</a></li>
+                </ul>
+
+                <strong>Kriisipuhelin</strong> – Vaikea elämäntilanne? Itsetuhoisia ajatuksia?
+                <ul>
+                    <li>Maksutonta keskusteluapua kriisissä olevilla ja heidän läheisilleen</li>
+                    <li>Apua saatavilla suomeksi, ruotsiksi, arabiaksi ja englanniksi (tarkista oikea numero)</li>
+                    <li>p. 01019 5202 (suomi)</li>
+                    <li><a href="https://mieli.fi/fi/tukea-ja-apua/kriisipuhelin-keskusteluapua-numerossa-09-2525-0111">Kriisipuhelin</a></li>
+                </ul>
+
+                <strong>Irti huumeista</strong> – käyttäjille/heidän läheisilleen
+                <ul>
+                    <li>Maksutonta tukea ja tietoa</li>
+                    <li>p. 0800 980 66</li>
+                    <li><a href="https://irtihuumeista.fi/ ">Irti huumeista</a></li>
+                </ul>
+
+                <h2>TAI TULE MUKAAN <strong>KOHTAYS RY:N</strong> TOIMINTAAN! </h2>
+                <p>Täällä voit chillailla muiden nuorten kanssa ja tutustua uusiin ystäviin, puhua mieltä askarruttavista asioista tai tulla kurkkimaan menoa ihan vain uteliaisuudesta.
+                  Käy katsomassa tapahtumakalenteri nettisivuillamme <a href="https://www.kohtaus.info/">KOHTAUS RY</a></p>
+                <h2>TERVETULOA! 😊</h2>
+
+                <h2><strong>MUITA RYHMIÄ TUKE TARVITSEVILLE NUORILLE</strong></h2>
+                <strong>Löydä oma tarinasi</strong>
+                <ul>
+                  <li>Yksinäisyyttä kokeville 18-25 -vuotiaille nuorille</li>
+                  <li>Tukea elämäntilanteisiin vertaistuesta</li>
+                </ul>
+
+                <strong>Lasinen lapsuus</strong>
+                <ul>
+                  <li>Lapsuudessaan aikuisten päihdekäytöstä kärsineille nuorille</li>
+                </ul>
+
+                <p>Lisäinfoa saat täältä:
+                  <a href="https://www.mielenterveysseura.fi/fi/tukea-ja-apua/ryhmiss%C3%A4/ryhm%C3%A4t-tukea-tarvitseville-nuorille">Ryhmät tukea tarvitseville nuorille</a>
+                </p>
+              </div>
+
             </Aux>
 
           );
